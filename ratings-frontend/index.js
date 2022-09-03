752a468c11aafd47ac64808db683567c80cbf0a2
Jwatts82 <wattscodes@gmail.com>
2022-09-03 11:50:10 -0400

    add states show

--- ratings-frontend/index.js
+++ ratings-frontend/index.js
@@ -1,15 +1,52 @@
 const BASE_URL = 'http://localhost:3000'
 
 window.addEventListener("DOMContentLoaded", () => {
-    getRolls()
+    getStates()
 })
 
 
-function getRolls() {
-    let main = document.getElementById('rolls')
+function getStates() {
+    let states = document.getElementById('states')
+    states.innerHTML = ''
     fetch(BASE_URL + '/rolls')
     .then(res => res.json())
-    .then(rolls => {
-        console.log(rolls)
+    .then(states => {
+        states.map(state => {
+        states.innerHTML += `        
+        <li>
+            <a href="#" data-id="${state.id}">${state.name}</a>
+        </li>
+        `
+        })
+        attachClicksToLinks()
+    })}
+
+
+function attachClicksToLinks() {
+    const states = document.querySelectorAll('li a')
+    states.forEach(state => {
+        state.addEventListener('click', displayState)
     })
 }
+
+function displayState(e) {
+    console.log(e.target)
+    let id = e.target.dataset.id
+    let states = document.getElementById('states')
+    states.innerHTML = ''
+    fetch(BASE_URL + `/states/${id}`)
+    .then (res => res.json())
+    .then(state => {
+        states.innerHTML = `
+        <h3>${state.name}</h3>
+        `
+        // state.rolls.forEach(roll => {
+        //     console.log(roll)
+        // })
+    })
+}
+
+
+
+
+
